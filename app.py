6b9e096f29a565854cc28a04458c5d2825973010
Aakash Prabhu <aakash604@gmail.com>
2020-05-18 20:06:29 -0700

    add messenger bot framework code.

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request
+from pymessenger.bot import Bot
+
+
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+
+def receive_message():
+    if request.method == 'GET':
+        # Before allowing people to message your bot, Facebook has implemented 
+        # a verify token that confirms all requests that your bot receives 
+        # came from Facebook. 
+        token_sent = request.args.get("hub.verify_token")
+        return verify_fb_token(token_sent)
+    else:
+        output = request.get_json()
+        for event in output['entry']:
+            messaging = event['messaging']
+            for message in messaging:
+                if message.get('message'):
+                    recipient_id = message['sender']['id']
+                    if message['message'].get('text'):
+                        response_sent_text = get_message()
+                        send_message(recipient_id, response_sent_text)
+                    if message['message'].get('attachments'):
+                        response_sent_nontext = get_message()
+                        send_message(recipient_id, response_sent_nontext)
+    return "Message Processed"
+
+def verify_fb_token(token_sent):
+    # take token sent by facebook and verify it matches the verify token you
+    # sent. If they match, allow the request, else return an error 
+    if token_sent == VERIFY_TOKEN:
+        return request.args.get("hub.challenge")
+        return 'Invalid verification token'
+
+def send_message(recipient_id, response):
+    # sends user the text message provided via input response parameter
+    bot.send_text_message(recipient_id, response)
+    return "success"
+
+
+if __name__ == '__main__':
+    app.run()
