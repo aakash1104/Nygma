a9696a23cf8782341002a5bb4365fe4a3003e527
Aakash Prabhu <aakash604@gmail.com>
2020-05-19 19:33:57 -0700

    bot: finish up boilerplate

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from pymessenger.bot import Bot
 
 
 app = Flask(__name__)
+ACCESS_TOKEN = 'ACCESS_TOKEN'
+VERIFY_TOKEN = 'VERIFY_TOKEN'
+bot = Bot(ACCESS_TOKEN)
+
+
+
 @app.route('/', methods=['GET', 'POST'])
 
 def receive_message():
@@ -13,6 +19,7 @@ def receive_message():
         token_sent = request.args.get("hub.verify_token")
         return verify_fb_token(token_sent)
     else:
+        # Get message that user sent
         output = request.get_json()
         for event in output['entry']:
             messaging = event['messaging']
@@ -32,7 +39,11 @@ def verify_fb_token(token_sent):
     # sent. If they match, allow the request, else return an error 
     if token_sent == VERIFY_TOKEN:
         return request.args.get("hub.challenge")
-        return 'Invalid verification token'
+    return 'Invalid verification token'
+
+def get_message():
+    sample_responses = ["Hello!", "How are you?", "Some other test message"]
+    return random.choice(sample_responses)
 
 def send_message(recipient_id, response):
     # sends user the text message provided via input response parameter
